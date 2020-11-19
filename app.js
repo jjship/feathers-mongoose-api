86d34aa88354732d9f551516425dbeec22b8afde
Jacek Skrzypek <jjskrzypek@gmail.com>
2020-11-19 20:01:36 +0100

    copied from docs - live update works on client

--- app.js
+++ app.js
@@ -2,53 +2,65 @@ const feathers = require('@feathersjs/feathers');
 const express = require('@feathersjs/express');
 const socketio = require('@feathersjs/socketio');
 
+// A messages service that allows to create new
+// and return all existing messages
 class MessageService {
   constructor() {
     this.messages = [];
   }
 
   async find() {
+    // Just return all our messages
     return this.messages;
   }
 
   async create(data) {
+    // The new message is the data merged with a unique identifier
+    // using the messages length since it changes whenever we add one
     const message = {
       id: this.messages.length,
       text: data.text,
     };
+
+    // Add new message to the list
     this.messages.push(message);
+
     return message;
   }
 }
 
+// Creates an ExpressJS compatible Feathers application
 const app = express(feathers());
 
-//middlewares
+// Parse HTTP JSON bodies
 app.use(express.json());
+// Parse URL-encoded params
 app.use(express.urlencoded({ extended: true }));
+// Host static files from the current folder
 app.use(express.static(__dirname));
-
-//REST & socket configuration
+// Add REST API support
 app.configure(express.rest());
+// Configure Socket.io real-time APIs
 app.configure(socketio());
-
-//registering route
+// Register an in-memory messages service
 app.use('/messages', new MessageService());
-
+// Register a nicer error handler than the default Express one
 app.use(express.errorHandler());
 
-// setup socket channels
-app.on('connection', (connection) => {
-  app.channel('everybody').join(connection);
-});
+// Add any new real-time connection to the `everybody` channel
+app.on('connection', (connection) => app.channel('everybody').join(connection));
+// Publish all events to the `everybody` channel
+app.publish((data) => app.channel('everybody'));
 
-// publish events to channel
-app.publish(() => app.chanel('everybody'));
+// Start the server
+app
+  .listen(3030)
+  .on('listening', () =>
+    console.log('Feathers server listening on localhost:3030')
+  );
 
-app.listen(3030).on('listening', () => {
-  console.log('Feathers server listening on localhost:3030');
-});
-
+// For good measure let's create a message
+// So our API doesn't look so empty
 app.service('messages').create({
   text: 'Hello world from the server',
 });
