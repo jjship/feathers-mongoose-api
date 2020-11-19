04d5b75eafd9bfc56f6e13406da69e11d6db72d6
Jacek Skrzypek <jjskrzypek@gmail.com>
2020-11-19 18:51:51 +0100

    basic service created & registered in feathers

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 const feathers = require('@feathersjs/feathers');
 const app = feathers();
+
+class MessageService {
+  constructor() {
+    this.messages = [];
+  }
+
+  async find() {
+    return this.messages;
+  }
+
+  async create(data) {
+    const message = {
+      id: this.messages.length,
+      text: data.text,
+    };
+    this.messages.push(message);
+    return message;
+  }
+}
+
+app.use('messages', new MessageService());
